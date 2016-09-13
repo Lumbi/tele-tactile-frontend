3b1b43bcd0db37580e3cb6e4c3cb66c23863006d
benoit12 <benoit.seguin.29@gmail.com>
2016-09-13 16:40:33 -0400

    Version 2

--- assets/js/to-buy.js
+++ assets/js/to-buy.js
@@ -23,27 +23,13 @@ var toBuy = [
 ];
 
 function buildList() {
-    var html = '';
-    toBuy.forEach(function (item) {
-        html =
-            '<tr>' +
-            '<td>' +
-            '<label class="checkbox">' +
-            '<input type="checkbox" value="" data-toggle="checkbox" onclick="check(' + item.Id + ')>' +
-            '</label>' +
-            '</td>' +
-            '<td>' + item.Name + '</td>' +
-            '<td class="td-actions text-right">' +
-            '<button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs" onclick="edit(' + item.Id + ')>' +
-            '<i class="fa fa-edit"></i>' +
-            '</button>' +
-            '<button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs" onclick="remove(' + item.Id + ')">' +
-            '<i class="fa fa-times"></i>' +
-            '</button>' +
-            '</td>' +
-            '</tr>'
+    $.get("assets/html/to-buy-template.html", function (source) {
+        var template = Handlebars.compile(source);
+
+        toBuy.forEach(function (item) {
+            $('#tobuy').append(template(item));
+        });
     });
-    return html;
 }
 
-document.getElementById('tobuy').innerHTML = buildList();
+buildList();
